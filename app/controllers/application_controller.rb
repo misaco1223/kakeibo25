bd31ac92ee90efd61144dc4f182b5d807f8f2aa6
玉水望朝 <tamamizumisa@tamamizunozomuasanoMacBook-Air.local>
2025-01-24 18:34:31 +0900

    fixed: application.htmlにtailwindクラスを適用した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   helper_method :current_user, :logged_in?
   add_flash_types :success, :danger
+  before_action :set_greeting
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
@@ -14,5 +15,17 @@ class ApplicationController < ActionController::Base
 
   def require_login
     redirect_to login_path, alert: "ログインが必要です" unless logged_in?
+  end
+
+  private
+  def set_greeting
+    current_time = Time.now
+    if (Time.parse("05:00")...Time.parse("10:00")).cover?(current_time)
+      @greeting = "おはようございます!"
+    elsif (Time.parse("10:00")...Time.parse("17:00")).cover?(current_time)
+      @greeting = "こんにちは!"
+    else
+      @greeting = "こんばんは!"
+    end
   end
 end
